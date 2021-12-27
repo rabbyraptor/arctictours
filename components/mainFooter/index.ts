dc940b76b00a33cdb185179c4322771086c528eb
Mathias Hammer <rabbyraptor@hotmail.com>
2021-12-27 10:32:44 +0100

    Add MainFooter component

---
+++ components/mainFooter/index.ts
@@ -0,0 +1,5 @@
+import { Vue } from "nuxt-property-decorator";
+
+export default class MainHeader extends Vue {
+	data = "footer content";
+}
---
+++ components/mainFooter/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>{{ data }}</div>
+</template>
+
+<script src="./index.ts"></script>
+<style src="./index.scss" lang="scss"></style>
