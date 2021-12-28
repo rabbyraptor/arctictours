0485f32bdc9942bc4c385777e1dbdb5645f0a99a
Mathias Hammer <rabbyraptor@hotmail.com>
2021-12-28 19:44:01 +0100

    Change header

--- components/mainHeader/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>{{ data }}</div>
-</template>
-
-<script lang="ts" src="./index.ts"></script>
-<style lang="scss" scoped src="./index.scss" />
---
+++ components/mainHeader/mainHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="logo">LOGO</div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script src="./mainHeader.ts"></script>
+<style src="./mainHeader.scss" lang="scss"></style>
