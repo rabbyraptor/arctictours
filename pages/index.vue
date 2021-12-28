bf3a64f6e25605c05dbd990009b0c5269d4874ca
Mathias Hammer <rabbyraptor@hotmail.com>
2021-12-28 19:49:21 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Home</div>
+  <div class="main-content">
+    <div class="container">
+      <div class="row">
+        <HeroImage />
+        <div class="col-10 offset-1">Col 6</div>
+        <div class="col-6">Col 6</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" src="./index.ts"></script>
