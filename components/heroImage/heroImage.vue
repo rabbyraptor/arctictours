9d77b9d5d558f063f96fd602e7b8b7cac22bbb20
Mathias Hammer <rabbyraptor@hotmail.com>
2021-12-28 19:49:13 +0100

    Add heroImage component

---
+++ components/heroImage/heroImage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container hero">
+    <div class="row">
+      <div class="col-6">Hero IMAGE</div>
+    </div>
+  </div>
+</template>
+
+<script src="./heroImage.ts"></script>
+<style src="./heroImage.scss" lang="scss"></style>
