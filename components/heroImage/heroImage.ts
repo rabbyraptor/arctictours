af807e6cda1cf7ae670aa2a384c597c2ea4ec570
Mathias Hammer <rabbyraptor@hotmail.com>
2022-01-04 16:39:04 +0100

    Update heroImage.ts

---
+++ components/heroImage/heroImage.ts
@@ -0,0 +1,7 @@
+import { Prop } from 'nuxt-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HeroImage extends Vue {
+    @Prop() readonly data!: HeroImage | undefined
+}
