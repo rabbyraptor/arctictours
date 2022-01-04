6edcbcfc2de34d88280c81ea059780c0d00c2e61
Mathias Hammer <rabbyraptor@hotmail.com>
2022-01-04 16:40:21 +0100

    Update index.ts

--- pages/index.ts
+++ pages/index.ts
@@ -1,3 +1,15 @@
-import Vue from 'vue'
+import { Component, Vue } from 'vue-property-decorator'
 
-export default Vue.extend({})
+@Component
+export default class HomePage extends Vue {
+    data = {
+        hero: {
+            image: '/images/dog-sledding/dogsledge-1.jpg',
+            text: 'Looking for an adventure? <br />You have come to the right place.',
+            cta: {
+                link: '#',
+                text: 'See tours'
+            }
+        }
+    }
+}
