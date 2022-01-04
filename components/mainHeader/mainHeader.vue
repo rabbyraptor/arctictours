9d7a2d8dc184025c76e4738b6a5068b1b3204bf2
Mathias Hammer <rabbyraptor@hotmail.com>
2022-01-04 16:39:16 +0100

    Update mainHeader.vue

--- components/mainHeader/mainHeader.vue
+++ components/mainHeader/mainHeader.vue
@@ -1,8 +1,9 @@
 <template>
   <header>
     <div class="container">
-      <div class="row">
-        <div class="logo">LOGO</div>
+      <div class="row p-4">
+        <nuxt-link to="/" class="col-2 offset-1 logo"> Arctic Tours </nuxt-link>
+        <div class="col-1 offset-7">Burger</div>
       </div>
     </div>
   </header>
