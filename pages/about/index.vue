3d4817d47918f3ecb5daa0e37fb176ad2b6622b3
Mathias Hammer <rabbyraptor@hotmail.com>
2022-01-06 22:36:52 +0100

    Add about page

---
+++ pages/about/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="main-grid">
+      <div class="container">
+        <div class="row">About</div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script src="./index.ts"></script>
