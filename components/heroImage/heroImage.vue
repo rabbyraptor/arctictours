dce391caad9a8bf0b0f8b9d46934dc981e007dd5
Mathias Hammer <rabbyraptor@hotmail.com>
2022-01-31 17:42:52 +0100

    Add bg color to hero image

--- components/heroImage/heroImage.vue
+++ components/heroImage/heroImage.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="container">
     <div class="row">
-      <div :style="'background-image: url(' + data.image + ')'" class="hero">
-        <h1 class="col-lg-10 offset-lg-1 hero__text" v-html="data.text" />
+      <div
+        :style="
+          'background: linear-gradient(to bottom, #e45a5a8a, #e45a5a8a), url(' +
+          data.image.fields.file.url +
+          ') no-repeat center'
+        "
+        class="hero"
+      >
+        <h1 class="col-lg-10 offset-lg-1 hero__text">
+          <span v-for="(p, index) in data.text.content"
+            >{{ p.content[0].value }}<br
+          /></span>
+        </h1>
         <div class="hero__cta">
           <CtaButton :data="data.cta" />
         </div>
