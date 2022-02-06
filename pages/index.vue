47158ab9c268b5b37053e7513ce70c58f2914b44
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-06 10:29:04 +0100

    Add homePage content to Contentful

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,9 @@
       <div class="row teaser-block">
         <div class="col-lg-4 offset-lg-1 d-flex align-items-center">
           <div class="teaser-block__text">
-            <h2>Experience nature.</h2>
+            <h2>{{ data.teaserBlock1.fields.title }}</h2>
             <p>
-              Kangerlussuaq has more than 300 days of clear sky a year, making
-              it one of the best places to see the northern lights which are
-              visible between October and April.
+              {{ data.teaserBlock1.fields.text }}
             </p>
             <CtaButton
               class="mt-4"
@@ -22,23 +20,29 @@
         </div>
         <div
           class="col-lg-6 offset-lg-1 teaser-block__image"
-          style="background-image: url('/images/northern-lights-1.jpg')"
+          :style="
+            'background-image: url(' +
+            data.teaserBlock1.fields.image.fields.file.url +
+            ')'
+          "
         />
       </div>
       <div class="row teaser-block">
         <nuxt-link
           to="/tours/ice-sheet"
           class="col-lg-6 teaser-block__image"
-          style="background-image: url('/images/ice-sheet/ice-sheet-2.jpg')"
+          :style="
+            'background-image: url(' +
+            data.teaserBlock2.fields.image.fields.file.url +
+            ')'
+          "
         />
 
         <div class="col-lg-4 offset-lg-1 d-flex align-items-center">
           <div class="teaser-block__text">
-            <h2>Get close. Real close.</h2>
+            <h2>{{ data.teaserBlock2.fields.title }}</h2>
             <p>
-              The Greenland Ice Cap is a mecca for travelers who wish to see a
-              true wonder of the world. Kangerlussuaq offers easy access to the
-              Greenland Ice Cap, via a 25 km dirt road.
+              {{ data.teaserBlock2.fields.text }}
             </p>
             <CtaButton
               class="mt-4"
@@ -53,10 +57,9 @@
       <div class="row teaser-block">
         <div class="col-lg-4 offset-lg-1 d-flex align-items-center">
           <div class="teaser-block__text">
-            <h2>Majestic. Wild. Life.</h2>
+            <h2>{{ data.teaserBlock3.fields.title }}</h2>
             <p>
-              Surround yourself by muskoxen and caribou with outstanding
-              opportunity for amazing pictures of the Arctic wildlife.
+              {{ data.teaserBlock3.fields.text }}
             </p>
             <CtaButton
               class="mt-4"
@@ -70,7 +73,11 @@
         <nuxt-link
           to="/tours/wildlife"
           class="col-lg-6 offset-lg-1 teaser-block__image"
-          style="background-image: url('/images/wildlife/muskox-1.jpg')"
+          :style="
+            'background-image: url(' +
+            data.teaserBlock3.fields.image.fields.file.url +
+            ')'
+          "
         />
       </div>
     </div>
