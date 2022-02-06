307c8151c81bfd90c6462fb9f03ef7b2540327de
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-06 10:27:52 +0100

    Update burgerMenu.ts

--- components/burgerMenu/burgerMenu.ts
+++ components/burgerMenu/burgerMenu.ts
@@ -2,10 +2,10 @@ import { Component, Vue, Watch } from 'vue-property-decorator'
 
 @Component
 export default class BurgerMenu extends Vue {
-    public isOpen: boolean = false;
+    public isOpen: boolean = true;
 
-    @Watch('close-menu', { immediate: true, deep: true })
-    closeMenu() {
-        // this.isOpen = !this.isOpen
+    @Watch('close', { immediate: true, deep: true })
+    close() {
+        this.isOpen = !this.isOpen
     }
 }
