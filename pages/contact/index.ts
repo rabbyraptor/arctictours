4331e4df0c83a996575f87e07a642de8e2fb9c8f
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-07 00:25:24 +0100

    Add contact page

--- pages/contact/index.ts
+++ pages/contact/index.ts
@@ -1,6 +1,36 @@
 import { Component, Vue } from 'vue-property-decorator'
+const contentful = require('contentful')
+const client = contentful.createClient({
+    space: 'pzbhz3gvlzr3',
+    accessToken: 'b-5XP09OllbfKyrmgfE-LmN1xRurBNKm3eVUCGklIzo'
+})
+const contentfulClient = client
+@Component({
+    async asyncData() {
+        return Promise.all([
+            await contentfulClient.getEntries({
+                content_type: 'contactPage',
+                include: 10,
+                order: 'sys.id'
+            })
+        ]).then(([pages]) => {
+            return {
+                data: pages.items[0].fields
+            }
+        }).catch(console.error)
+    }
+})
 
-@Component
 export default class ContactPage extends Vue {
+    public isSubmitted: boolean = false;
+    public submitButtonText: string = 'Submit';
 
+    submitForm() {
+        this.isSubmitted = true
+        this.submitButtonText = 'Sending...'
+        setTimeout(() => { this.submitButtonText = 'Sent!' }, 800);
+        (document.getElementById('name')! as HTMLInputElement).value = '';
+        (document.getElementById('email')! as HTMLInputElement).value = '';
+        (document.getElementById('message')! as HTMLInputElement).value = ''
+    }
 }
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,11 +1,57 @@
 <template>
-  <main>
-    <div class="main-grid">
-      <div class="container">
-        <div class="row">Contact</div>
+  <div v-if="data != null" class="main-grid">
+    <div class="container">
+      <div class="row tour-intro py-5">
+        <div class="col-lg-4 offset-lg-1 d-flex align-items-center">
+          <div class="row">
+            <div class="split-block__text">
+              <h2>
+                {{ data.title }}
+              </h2>
+              <p v-for="p in data.text.content">
+                <span v-html="p.content[0].value" />
+              </p>
+            </div>
+            <div class="contact-form py-5">
+              <h3>Write a message</h3>
+              <form>
+                <label for="name">
+                  <h4>Your name</h4>
+                </label>
+                <input id="name" type="text" /><br />
+                <label for="email">
+                  <h4>Your email</h4>
+                </label>
+                <input id="email" type="text" /><br />
+                <label for="message"> <h4>Your message</h4></label>
+                <textarea id="message" type="textarea" /><br />
+                <CtaButton
+                  class="mt-4"
+                  :data="{
+                    text: submitButtonText,
+                    linkDestination: '',
+                  }"
+                  @click.native="submitForm()"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 offset-lg-1">
+          <div class="row">
+            <div
+              class="split-block__image"
+              :style="
+                'background-image: url(' +
+                data.image.fields.file.url +
+                '?fm=webp)'
+              "
+            />
+          </div>
+        </div>
       </div>
     </div>
-  </main>
+  </div>
 </template>
 
 <script src="./index.ts"></script>
