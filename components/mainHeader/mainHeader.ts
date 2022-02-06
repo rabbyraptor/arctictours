0c3347fe9f30329dc11313f7f3370c73386bc1e9
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-06 10:28:23 +0100

    Adjusted header for mobile

--- components/mainHeader/mainHeader.ts
+++ components/mainHeader/mainHeader.ts
@@ -1,11 +1,6 @@
 import { Component, Vue } from 'vue-property-decorator'
-// const Slide = require('vue-burger-menu')
-// import { Slide } from 'vue-burger-menu'
-
 @Component({
-    components: {
-        // Slide
-    }
+
 })
 export default class MainHeader extends Vue {
 
--- components/mainHeader/mainHeader.vue
+++ components/mainHeader/mainHeader.vue
@@ -2,15 +2,15 @@
   <header>
     <div class="container">
       <div class="row p-4">
-        <nuxt-link to="/" class="col-xl-2 col-lg-8 col-5 offset-xl-1 logo">
+        <nuxt-link to="/" class="col-xl-3 col-lg-8 col-5 offset-xl-1 logo">
           Arctic Tours
         </nuxt-link>
         <div
-          class="col-xl-2 col-lg-4 col-7 offset-xl-6 d-flex align-items-center justify-content-end"
+          class="col-xl-2 col-lg-4 col-7 offset-xl-5 d-flex align-items-center justify-content-end"
         >
           <NavMenu class="desktop-nav-menu" />
 
-          <BurgerMenu>
+          <BurgerMenu class="mobile-nav-menu">
             <NavMenu />
           </BurgerMenu>
         </div>
