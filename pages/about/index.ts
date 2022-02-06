ec3a2989e331fc64af764e66ae4651fa363ca40c
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-07 00:25:14 +0100

    Add about page

--- pages/about/index.ts
+++ pages/about/index.ts
@@ -1,6 +1,26 @@
 import { Component, Vue } from 'vue-property-decorator'
+const contentful = require('contentful')
+const client = contentful.createClient({
+    space: 'pzbhz3gvlzr3',
+    accessToken: 'b-5XP09OllbfKyrmgfE-LmN1xRurBNKm3eVUCGklIzo'
+})
+const contentfulClient = client
+@Component({
+    async asyncData() {
+        return Promise.all([
+            await contentfulClient.getEntries({
+                content_type: 'aboutPage',
+                include: 10,
+                order: 'sys.id'
+            })
+        ]).then(([pages]) => {
+            return {
+                data: pages.items[0].fields
+            }
+        }).catch(console.error)
+    }
+})
 
-@Component
 export default class AboutPage extends Vue {
 
 }
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <main>
-    <div class="main-grid">
-      <div class="container">
-        <div class="row">About</div>
+  <div v-if="data != null" class="main-grid">
+    <div class="container">
+      <div class="row tour-intro">
+        <div class="col-lg-4 offset-lg-1 d-flex align-items-center">
+          <div class="split-block__text">
+            <h2>
+              {{ data.title }}
+            </h2>
+            <p v-for="p in data.text.content">
+              {{ p.content[0].value }}
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-6 offset-lg-1 split-block__preview-grid">
+          <div class="row">
+            <div
+              v-for="image in data.images"
+              :key="image.key"
+              class="col-6 split-block__image"
+              :style="
+                'background-image: url(' + image.fields.file.url + '?fm=webp)'
+              "
+            />
+          </div>
+        </div>
       </div>
     </div>
-  </main>
+    <ContactCta :data="data.contactCta.fields" />
+  </div>
 </template>
 
 <script src="./index.ts"></script>
