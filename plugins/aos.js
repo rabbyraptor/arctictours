7377d3937c9b9204e7b0508daadf63c4d8530752
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-19 17:24:43 +0100

    Fix initial load animations

--- plugins/aos.js
+++ plugins/aos.js
@@ -2,11 +2,14 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 export default ({ app }) => {
-    app.AOS = new AOS.init({
-        disable: window.innerWidth < 640,
-        offset: 130,
-        duration: 600,
-        easing: 'ease-in-out-cubic',
-        once: true
-    }) // or any other options you need
+    setTimeout(() => {
+        app.AOS = new AOS.init({
+            disable: window.innerWidth < 640,
+            offset: 130,
+            duration: 600,
+            easing: 'ease-in-out-cubic',
+            once: true,
+            delay: 100
+        }) // or any other options you need
+    }, 0)
 }
