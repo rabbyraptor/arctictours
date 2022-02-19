85e2df1522b2f901f724fe828c4a2c985266ee4a
Mathias Hammer <rabbyraptor@hotmail.com>
2022-02-19 16:46:56 +0100

    Install aos

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,12 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '@/plugins/aos', mode: 'client' }
   ],
+
+  purgeCSS: {
+    whitelist: ['aos-init', 'aos-animate', 'data-aos-delay', 'data-aos-duration', 'fade-up', 'zoom-in']
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [{ path: '~/components', extensions: ['vue'] }],
